790f6a683fba7fb42c7b9ff57ce7f4094a961394
stan-dot <stanislawmalinowski09@gmail.com>
2022-09-12 20:52:07 +0200

    finished the tutorial

---
+++ options.js
@@ -0,0 +1,33 @@
+const page = document.getElementById('buttonDiv');
+const selectedClassName = 'current';
+const presetButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
+
+function handleButtonClick(event) {
+  const current = event.target.parentElement.querySelector(`.${selectedClassName}`)
+  if (current && current !== event.target) {
+    current.classList.remove(selectedClassName);
+  }
+  const color = event.target.dataset.color;
+  event.target.classList.add(selectedClassName);
+  chrome.storage.sync.set({ color });
+}
+
+
+function constructOptions(buttonColors) {
+  chrome.storage.sync.get('color', (data) => {
+    const currentColor = data.color;
+    for (const buttonColor of buttonColors) {
+      const button = document.createElement('button');
+      button.dataset.color = buttonColor;
+      button.style.backgroundColor = buttonColor;
+
+      if (buttonColor === currentColor) {
+        button.classList.add(selectedClassName);
+      }
+      button.addEventListener('click', handleButtonClick);
+      page.appendChild(button);
+    }
+  })
+}
+
+constructOptions(presetButtonColors);
---
+++ popup.js
@@ -0,0 +1,20 @@
+const changeColor = document.getElementById('changeColor');
+chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
+});
+
+
+changeColor.addEventListener('click', async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: setPageBackgroundColor,
+  });
+});
+
+
+function setPageBackgroundColor() {
+  chrome.storage.sync.get('color', ({ color }) => {
+    document.body.style.backgroundColor = color;
+  })
+}

be7d93c60bbc7b265bb085768db5481ce7f7ad91
stan-dot <stanislawmalinowski09@gmail.com>
2022-10-25 20:05:08 +0100

    run deno format

--- app/src/components/table/TableContextMenu.tsx
+++ app/src/components/table/TableContextMenu.tsx
@@ -1,45 +1,69 @@
-import { getChildrenLinks, openAllChildren } from "../../functions/ifHasChildrenFolders";
+import {
+  getChildrenLinks,
+  openAllChildren,
+} from "../../functions/ifHasChildrenFolders";
 
 /**
- * @param props 
- * @returns 
+ * @param props
+ * @returns
  */
-export function TableContextMenu(props: { thing: chrome.bookmarks.BookmarkTreeNode, position: number[], closeCallback: () => void }): JSX.Element {
-  const childrenLinks: chrome.bookmarks.BookmarkTreeNode[] = getChildrenLinks(props.thing);
+export function TableContextMenu(
+  props: {
+    thing: chrome.bookmarks.BookmarkTreeNode;
+    position: number[];
+    closeCallback: () => void;
+  },
+): JSX.Element {
+  const childrenLinks: chrome.bookmarks.BookmarkTreeNode[] = getChildrenLinks(
+    props.thing,
+  );
   const hasChildrenLinks: boolean = childrenLinks.length > 0;
   const tableContextMenuStyles: React.CSSProperties = {
-    position: 'absolute',
+    position: "absolute",
     left: `${props.position[0]}px`,
     right: `${props.position[1]}px`,
-    zIndex: '40'
+    zIndex: "40",
   };
-  return <div id="tableContextMenu" className="contextMenu" style={tableContextMenuStyles}>
-    <div className="group1">
-      <p>rename button</p>
-      <p>delete button</p>
+  return (
+    <div
+      id="tableContextMenu"
+      className="contextMenu"
+      style={tableContextMenuStyles}
+    >
+      <div className="group1">
+        <p>rename button</p>
+        <p>delete button</p>
+      </div>
+      <div className="group2">
+        <p>cut button</p>
+        <p>copy buton</p>
+        <p>paste buton</p>
+      </div>
+      <div className="group3">
+        <button
+          onClick={(e) => openAllChildren(props.thing)}
+          disabled={!hasChildrenLinks}
+        >
+          <p>open all {childrenLinks.length}</p>
+        </button>
+        <button
+          onClick={(e) => openAllChildren(props.thing, true)}
+          disabled={!hasChildrenLinks}
+        >
+          <p>open all {childrenLinks.length} in new window</p>
+        </button>
+        <button
+          onClick={(e) => openAllChildren(props.thing, true, true)}
+          disabled={!hasChildrenLinks}
+        >
+          <p>open all {childrenLinks.length} in Incognito winow</p>
+        </button>
+      </div>
+      <div className="group4">
+        <button onClick={() => props.closeCallback()}>
+          Close
+        </button>
+      </div>
     </div>
-    <div className="group2">
-      <p>cut button</p>
-      <p>copy buton</p>
-      <p>paste buton</p>
-
-    </div>
-    <div className="group3">
-      <button onClick={e => openAllChildren(props.thing)} disabled={!hasChildrenLinks}>
-        <p>open all {childrenLinks.length}</p>
-      </button>
-      <button onClick={e => openAllChildren(props.thing, true)} disabled={!hasChildrenLinks}>
-        <p>open all {childrenLinks.length} in new window</p>
-      </button>
-      <button onClick={e => openAllChildren(props.thing, true, true)} disabled={!hasChildrenLinks}>
-        <p>open all {childrenLinks.length} in Incognito winow</p>
-      </button>
-
-    </div>
-    <div className="group4">
-      <button onClick={() => props.closeCallback()}>
-        Close
-      </button>
-    </div>
-  </div>;
+  );
 }

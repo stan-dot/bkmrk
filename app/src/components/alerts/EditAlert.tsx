dddb2befb1e9376e3bb6c2853c56664b09318398
stan-dot <stanislawmalinowski09@gmail.com>
2023-02-01 12:00:27 +0000

    corrected z index

--- app/src/components/alerts/EditAlert.tsx
+++ app/src/components/alerts/EditAlert.tsx
@@ -25,12 +25,12 @@ export default function EditAlert(
   };
 
   return (
-    <div className="fixed backdrop-blur-md w-full h-full grid grid-cols-2 gap-4 place-content-center" id="alertBackground" style={{ display: `${visible ? 'absolute' : 'none'}` }}>
+    <div className="fixed backdrop-blur-md w-full h-full grid grid-cols-2 gap-4 place-content-center z-50" id="alertBackground" style={{ display: `${visible ? 'absolute' : 'none'}` }}>
       <form className="
       fixed top-1/3 left-1/3
       flex flex-col px-6 py-2
       m-auto
-      z-40 inset-0 border-solid border-gray-500 h-60 w-96  bg-slate-800 overflow-y-auto rounded  " id="editAlertForm" onSubmit={onSubmit}
+      z-60 inset-0 border-solid border-gray-500 h-60 w-96  bg-slate-800 overflow-y-auto rounded  " id="editAlertForm" onSubmit={onSubmit}
       // onBlur={closeCallback}
       >
         <h2 id="title" className="text-xl text-slate-50 m-4">Edit bookmark</h2>

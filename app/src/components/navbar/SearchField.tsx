663e48232337d5da935577b2a6f8bc7594544c89
stan-dot <stanislawmalinowski09@gmail.com>
2023-03-20 10:29:46 +0000

    updated documentation

--- app/src/components/navbar/SearchField.tsx
+++ app/src/components/navbar/SearchField.tsx
@@ -26,43 +26,52 @@ export function SearchField(props: {
   const isEmpty = value.length === 0;
 
   async function runSearch() {
+    console.log("starting the serach, term: ", value);
     const searchResults: chrome.bookmarks.BookmarkTreeNode[] = await chrome
       .bookmarks.search(value);
+    console.log("search results: ", searchResults);
     props.setDataCallback(searchResults);
   }
 
-  return <div className={`react-search-field bg-slate-800 rounded-full w-3/5 m-2 align-middle border-solid relative justify-between flex flex-row`} >
-    <div id="left-side-items" className="justify-start">
-      <button
-        className={`search-field-button h-fit  ${iconHighlight ? "text-slate-50" : "text-slate-400"
+  return (
+    <div
+      className={`react-search-field bg-slate-800 rounded-full w-3/5 m-2 align-middle border-solid relative justify-between flex flex-row`}
+    >
+      <div id="left-side-items" className="justify-start">
+        <button
+          className={`search-field-button h-fit  ${
+            iconHighlight ? "text-slate-50" : "text-slate-400"
           } align-middle cursor-pointer hover:bg-slate-50 p-3 pb-2 text-l border-color-white rounded-full}`}
+          type="button"
+          aria-label="search button"
+          onClick={onSearchClickHandler}
+          disabled={isEmpty}
+        >
+          &#128269;
+        </button>
+        <input
+          className="search-field-input lg:w-[300px] outline-none text-left bg-slate-800 border-none text-sm pb-4 text-slate-50 "
+          onChange={onChangeHandler}
+          onKeyDown={onEnterHandler}
+          onBlur={onBlurHandler}
+          placeholder={"Search bookmarks"}
+          type="text"
+          value={value}
+        />
+      </div>
+      <button
+        className={`search-field-button h-fit w-fit outline-none ${
+          isEmpty && "hidden"
+        } ${
+          iconHighlight ? "text-slate-50" : "text-slate-400"
+        } cursor-pointer p-2 text-xl rounded-full`}
         type="button"
-        aria-label="search button"
-        onClick={onSearchClickHandler}
+        aria-label="cancel button"
+        onClick={() => setValue("")}
         disabled={isEmpty}
       >
-        &#128269;
+        &#11199;
       </button>
-      <input
-        className="search-field-input lg:w-[300px] outline-none text-left bg-slate-800 border-none text-sm pb-4 text-slate-50 "
-        onChange={onChangeHandler}
-        onKeyDown={onEnterHandler}
-        onBlur={onBlurHandler}
-        placeholder={"Search bookmarks"}
-        type="text"
-        value={value}
-      />
     </div>
-    <button
-      className={`search-field-button h-fit w-fit outline-none ${isEmpty && "hidden"
-        } ${iconHighlight ? "text-slate-50" : "text-slate-400"
-        } cursor-pointer p-2 text-xl rounded-full`}
-      type="button"
-      aria-label="cancel button"
-      onClick={() => setValue("")}
-      disabled={isEmpty}
-    >
-      &#11199;
-    </button>
-  </div>
+  );
 }
